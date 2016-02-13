97587f6dfba42be51aa76ffda2c60479b72e2b5c
Maldercito <miky.rl@gmail.com>
2016-02-13 19:31:04 +0100

    a

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3;
+    public float rotspeed = 50;
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(new Vector2(0, speed * Time.deltaTime));
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(new Vector2(0, -speed * Time.deltaTime));
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(new Vector3(0, 0, rotspeed * Time.deltaTime));
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(new Vector3(0, 0, -rotspeed * Time.deltaTime));
+
+    }
+}
