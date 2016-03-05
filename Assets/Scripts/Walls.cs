2ba1a3bef79c25fbffddf503ea638de137c916e2
Maldercito <miky.rl@gmail.com>
2016-03-05 19:05:25 +0100

    prefabs

---
+++ Assets/Scripts/Walls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walls : MonoBehaviour
+{
+    [SerializeField]
+    GameObject explosion;
+
+    [SerializeField]
+    private int life = 2;
+    // Use this for initialization
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerBullet"))
+        {
+            Destroy(collision.gameObject);
+            life--;
+            if (life <= 0)
+            {
+                gameObject.SetActive(false);
+                Instantiate(explosion, transform.position, Quaternion.identity);
+            }
+
+        }
+    }
+}
