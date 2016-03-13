6d480f2bf29b4ea5c26e2dbb4e4ee5e1516ac40e
Maldercito <miky.rl@gmail.com>
2016-03-13 17:03:58 +0100

    fixes

---
+++ Assets/Scripts/HardWall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HardWall : MonoBehaviour
+{
+ 
+
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerBullet") || collision.gameObject.CompareTag("EnemyBullet"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
